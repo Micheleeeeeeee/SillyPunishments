5d576ac556ad40789a61b51185e23234c25a0a3f
Silly Sock <sillysockcodes@gmail.com>
2021-09-19 18:09:55 +0930

    Revamp C.java

--- src/main/java/me/sillysock/sillypunishments/sillyapi/C.java
+++ src/main/java/me/sillysock/sillypunishments/sillyapi/C.java
@@ -4,22 +4,29 @@ import org.bukkit.ChatColor;
 
 public class C {
 
-    public static final ChatColor GREEN = ChatColor.GREEN;
-    public static final ChatColor DARK_GREEN = ChatColor.DARK_GREEN;
-    public static final ChatColor BOLD = ChatColor.BOLD;
-    public static final ChatColor RESET = ChatColor.RESET;
-    public static final ChatColor RED = ChatColor.RED;
-    public static final ChatColor DARK_RED = ChatColor.DARK_RED;
-    public static final ChatColor PINK = ChatColor.LIGHT_PURPLE;
-    public static final ChatColor PURPLE = ChatColor.DARK_PURPLE;
-    public static final ChatColor GOLD = ChatColor.GOLD;
-    public static final ChatColor YELLOW = ChatColor.YELLOW;
-    public static final ChatColor AQUA = ChatColor.AQUA;
-    public static final ChatColor DARK_AQUA = ChatColor.DARK_AQUA;
-    public static final ChatColor GRAY = ChatColor.GRAY;
-    public static final ChatColor DARK_GRAY = ChatColor.DARK_GRAY;
-    public static final ChatColor DARK_BLUE = ChatColor.DARK_BLUE;
-    public static final ChatColor BLUE = ChatColor.BLUE;
+    public static final char COLOR_CHAR = '§';
+    public static final String BOLD = "§l";
+    public static final String STRIKE = "§m";
+    public static final String UNDERLINE = "§n";
+    public static final String MAGIC = "§k";
+    public static final String ITALIC = "§o";
+    public static final String RESET = "§r";
+    public static final String BLACK = "§0";
+    public static final String DARK_BLUE = "§1";
+    public static final String DARK_GREEN = "§2";
+    public static final String DARK_AQUA = "§3";
+    public static final String DARK_RED = "§4";
+    public static final String DARK_PURPLE = "§5";
+    public static final String GOLD = "§6";
+    public static final String GRAY = "§7";
+    public static final String DARK_GRAY = "§8";
+    public static final String BLUE = "§9";
+    public static final String GREEN = "§a";
+    public static final String AQUA = "§b";
+    public static final String RED = "§c";
+    public static final String LIGHT_PURPLE = "§d";
+    public static final String YELLOW = "§e";
+    public static final String WHITE = "§f";
 
 
     public static String format(final String s) {
